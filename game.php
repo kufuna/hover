a09474fca5d6bd52c96fc47740b40da1247cce84
dubrovsky <33762730+kufuna@users.noreply.github.com>
2019-03-22 13:59:53 +0400

    Add files via upload

---
+++ game.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+<div id="game"></div>
+
+
+
+<script src="js/game.js"></script>
+</body>
+</html>
